a5a1769ae53b2f48d882fc77352f9f750120f816
Chad Crowe <ccrowe@ccrowe-mn1.linkedin.biz>
2019-12-29 10:01:58 -0800

    adding an efficient implementation

--- src/hello_world/core.clj
+++ src/hello_world/core.clj
@@ -3,7 +3,21 @@
 (defn -main [& args]
   (println "Hello, World!"))
 
-(defn rotate-element [elements remaining-rotations-count]
+(defn rotate-element
+  ([unrotated remaining-rotations-count]
+   (rotate-element unrotated (vector) remaining-rotations-count)
+   )
+  ([unrotated rotating remaining-rotations-count]
+  (if (= remaining-rotations-count 0)
+    (concat unrotated rotating)
+    (let [front (first unrotated)
+          remaining (rest unrotated)
+          building (conj rotating front)
+          updated-remaining-rotations (- remaining-rotations-count 1)]
+      (rotate-element remaining building updated-remaining-rotations))))
+  )
+
+(defn rotate-element-slow [elements remaining-rotations-count]
   (if (= remaining-rotations-count 0)
     elements
     (let [front (first elements)
@@ -12,7 +26,7 @@
           rotated (conj as-v front)
           as-l (into () rotated)
           updated-remaining-rotations (- remaining-rotations-count 1)]
-      (rotate-element as-l updated-remaining-rotations))))
+      (rotate-element-slow as-l updated-remaining-rotations))))
 
 (defn rotLeft [a d]
   (rotate-element a d))
